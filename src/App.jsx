6a65fb1c96da3288cadba9722ec4f6b8aeb62ce3
Damon <jiangh@servyou.com.cn>
2020-10-15 22:26:53 +0800

    feat: module-federation

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import Header from "header/Header";
+
 import "./index.css";
 
-const App = () => <div>Hi there, I'm React from Webpack 5.</div>;
+const App = () => (
+  <div>
+    <Header/>
+    I'm the Consumer App.
+  </div>
+);
 
 ReactDOM.render(<App />, document.getElementById("app"));
